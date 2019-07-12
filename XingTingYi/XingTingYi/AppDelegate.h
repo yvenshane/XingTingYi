2855de9b9fe7c1f31d4174456571f21847bb2e86
YVEN SHANE <yvenshane@aol.com>
2019-07-12 11:20:07 +0800

    Create project

---
+++ XingTingYi/XingTingYi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XingTingYi
+//
+//  Created by YVEN on 2019/7/12.
+//  Copyright © 2019 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XingTingYi/XingTingYi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XingTingYi
+//
+//  Created by YVEN on 2019/7/12.
+//  Copyright © 2019 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
